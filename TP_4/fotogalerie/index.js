0c00344d1925b7ca86ff3ca6e93c3b07f8454dd4
JitkaM <zoa@seznam.cz>
2021-07-11 14:10:08 +0200

    popisky

--- TP_4/fotogalerie/index.js
+++ TP_4/fotogalerie/index.js
@@ -1,41 +1,44 @@
 const images = [
     { id: 0,
       title: 'Lední medvědi', 
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, nemo doloribus maiores qui voluptate delectus.',
+      description: 'Medvědi Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, nemo doloribus maiores qui voluptate delectus.',
       src: 'images/polar-bear.jpg' },
     { id: 1,
       title: 'Orel',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, nemo doloribus maiores qui voluptate delectus.',
+      description: 'Orel Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, nemo doloribus maiores qui voluptate delectus.',
       src: 'images/eagle.jpg' },
     { id: 2,
       title: 'Slon',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, nemo doloribus maiores qui voluptate delectus.',
+      description: 'Slon Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, nemo doloribus maiores qui voluptate delectus.',
       src: 'images/elephant.jpg'},
     { id: 3,
       title: 'Koně',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, nemo doloribus maiores qui voluptate delectus.',
+      description: 'Koně Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, nemo doloribus maiores qui voluptate delectus.',
       src: 'images/horses.jpg' },
     { id: 4,
       title: 'Hora',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, nemo doloribus maiores qui voluptate delectus.',
+      description: 'Hora Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, nemo doloribus maiores qui voluptate delectus.',
       src: 'images/mountain.jpg' },
     { id: 5,
       title: 'Strom',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, nemo doloribus maiores qui voluptate delectus.',
+      description: 'Strom Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, nemo doloribus maiores qui voluptate delectus.',
       src: 'images/tree.jpg' },
     { id: 6,
       title: 'Velryba',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, nemo doloribus maiores qui voluptate delectus.',
+      description: 'Velryba Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, nemo doloribus maiores qui voluptate delectus.',
       src: 'images/whale.jpg' },
     { id: 7,
       title: 'Vlci',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, nemo doloribus maiores qui voluptate delectus.',
+      description: 'Vlci Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, nemo doloribus maiores qui voluptate delectus.',
       src: 'images/wolfs.jpg'}              
 ];
 
 const gallery = document.querySelector('.gallery');
 const big = document.querySelector('.big-foto');
 const img = document.querySelector('.big-foto img');
+const bigTitle = document.querySelector('.desc h1');
+const bigDesc = document.querySelector('.desc p');
+
 
 function createGalery(arr){
     arr.forEach(element => {
@@ -55,6 +58,8 @@ function makeBigger(){
   whiteBorder.removeAttribute('id');
   this.id = 'white-border';
   img.src = this.src;
+  bigTitle.innerHTML = images[this.className].title;
+  bigDesc.innerHTML = images[this.className].description;
   img.className = '';
   img.classList.add(this.className);
 }
@@ -68,6 +73,8 @@ right.addEventListener('click', function () {
       number++;
       img.className = '';
       img.src = images[number].src;
+      bigTitle.innerHTML = images[number].title;
+      bigDesc.innerHTML = images[number].description;
       document.querySelector(`[class="${number}"]`).id = 'white-border';
       img.classList.add(images[number].id);
      }
@@ -82,6 +89,8 @@ left.addEventListener('click', function () {
    number--;
    img.className = '';
    img.src = images[number].src;
+   bigTitle.innerHTML = images[number].title;
+   bigDesc.innerHTML = images[number].description;
    document.querySelector(`[class="${number}"]`).id = 'white-border';
    img.classList.add(images[number].id);
   }
